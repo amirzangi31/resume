3b84ac2d2a4d9409bcbcbcf73efeff0a8cdc6eec
amirmohammad zangiabadi <zangiabadi1378888@gmail.com>
2023-11-01 12:26:03 +0330

    test

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import { NextIntlClientProvider } from "next-intl";
-import AnimatedCursor from "react-animated-cursor"
+// import AnimatedCursor from "react-animated-cursor"
 const Layout = async ({ children, local }) => {
   let messages;
   try {
@@ -12,7 +12,7 @@ const Layout = async ({ children, local }) => {
   }
   return (
     <NextIntlClientProvider locale={local} messages={messages}>
-      <AnimatedCursor />
+      {/* <AnimatedCursor /> */}
       <Header />
       <main>
         <div className="container mx-auto p-4">{children}</div>
